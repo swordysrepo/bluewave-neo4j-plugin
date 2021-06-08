6ddfe1dcb92819ea547b60c72dbb8daeab7409e9
JohnKim <john.kim@culmen.com>
2021-06-08 09:13:06 -0400

    cleaned up some unused code

--- src/main/java/bluewave/neo4j/plugins/Neo4JTransactionEventListener.java
+++ src/main/java/bluewave/neo4j/plugins/Neo4JTransactionEventListener.java
@@ -95,10 +95,6 @@ public class Neo4JTransactionEventListener implements TransactionEventListener<O
 
 
         if (data.createdNodes() != null) {
-
-
-            Files.writeString(logFilePath, "\n///Created Nodes: " + data.createdNodes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-            nodeName = data.createdNodes().toString();
             type = "NODE";
             transactionType = TransactionType.CREATION;
 
@@ -108,16 +104,12 @@ public class Neo4JTransactionEventListener implements TransactionEventListener<O
 
         }
         if (data.deletedNodes() != null) {
-            Files.writeString(logFilePath, "\n///Deleted Nodes: " + data.deletedNodes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-            nodeName = data.deletedNodes().toString();
             type = "NODE";
             transactionType = TransactionType.DELETION;
 
             dataString += "Deleted Nodes | " + data.deletedNodes() + " | ";
         }
         if (data.createdRelationships() != null) {
-            Files.writeString(logFilePath, "\n///Created Relationships: " + data.createdRelationships(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-            relName = data.createdRelationships().toString();
 
             type = "RELATIONSHIP";
             transactionType = TransactionType.CREATION;
@@ -125,8 +117,6 @@ public class Neo4JTransactionEventListener implements TransactionEventListener<O
             dataString += "Created Relationships | " + data.createdRelationships() + " | ";
         }
         if (data.deletedRelationships() != null) {
-            Files.writeString(logFilePath, "\n///Deleted Relationships: " + data.deletedRelationships(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-            relName = data.deletedRelationships().toString();
 
             type = "RELATIONSHIP";
             transactionType = TransactionType.DELETION;
@@ -135,8 +125,6 @@ public class Neo4JTransactionEventListener implements TransactionEventListener<O
 
         }
         if (data.assignedLabels() != null) {
-            Files.writeString(logFilePath, "\n///Assigned Labels:  " + data.assignedLabels(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-            labelName = data.assignedLabels().toString();
 
             type = "LABEL";
             transactionType = TransactionType.LABEL_ASSIGNMENT;
@@ -144,8 +132,6 @@ public class Neo4JTransactionEventListener implements TransactionEventListener<O
             dataString += "Assigned Labels | " + data.assignedLabels() + " | ";
         }
         if (data.removedLabels() != null) {
-            Files.writeString(logFilePath, "\n///Removed Labels: " + data.removedLabels(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-            labelName = data.removedLabels().toString();
             type = "LABEL";
             transactionType = TransactionType.LABEL_REMOVAL;
 
@@ -153,76 +139,32 @@ public class Neo4JTransactionEventListener implements TransactionEventListener<O
         }
         if (data.assignedNodeProperties() != null) {
 
-            List<String> keys = new ArrayList<String>();
-            List<String> values = new ArrayList<String>();
-
-
-            for (PropertyEntry<Node> assignedNodeProperty : data.assignedNodeProperties()) {
-                keys.add(assignedNodeProperty.key());
-                values.add(assignedNodeProperty.value().toString());
-
-            }
-
             transactionType = TransactionType.PROPERTY_ASSIGNMENT;
             type = "NODE";
-            propName = keys.stream().collect(Collectors.joining(","));
-            propValue = values.stream().collect(Collectors.joining(","));
 
             dataString += "Assigned Node Properties | " + data.assignedNodeProperties().toString() + " | ";
 
         }
         if (data.removedNodeProperties() != null) {
 
-            List<String> keys = new ArrayList<String>();
-            List<String> values = new ArrayList<String>();
-
-
-            for (PropertyEntry<Node> removedNodeProperty : data.removedNodeProperties()) {
-                keys.add(removedNodeProperty.key());
-                values.add(removedNodeProperty.value().toString());
-
-            }
-
             transactionType = TransactionType.PROPERTY_REMOVAL;
             type = "NODE";
-            propName = keys.stream().collect(Collectors.joining(","));
-            propValue = values.stream().collect(Collectors.joining(","));
 
             dataString += "Removed Node Properties | " + data.removedNodeProperties().toString() + " | ";
 
         }
         if (data.assignedRelationshipProperties() != null) {
 
-            List<String> keys = new ArrayList<String>();
-            List<String> values = new ArrayList<String>();
-
-            for (PropertyEntry<Relationship> assignedRelationshipProperty : data.assignedRelationshipProperties()) {
-                keys.add(assignedRelationshipProperty.key());
-                values.add(assignedRelationshipProperty.value().toString());
-            }
-
             transactionType = TransactionType.PROPERTY_ASSIGNMENT;
             type = "RELATIONSHIP";
-            propName = keys.stream().collect(Collectors.joining(","));
-            propValue = values.stream().collect(Collectors.joining(","));
 
             dataString += "Assigned Relationship Properties | " + data.assignedRelationshipProperties().toString() + " | ";
 
         }
         if (data.removedRelationshipProperties() != null) {
 
-            List<String> keys = new ArrayList<String>();
-            List<String> values = new ArrayList<String>();
-
-            for (PropertyEntry<Relationship> removedRelationshipProperty : data.removedRelationshipProperties()) {
-                keys.add(removedRelationshipProperty.key());
-                values.add(removedRelationshipProperty.value().toString());
-            }
-
             transactionType = TransactionType.PROPERTY_REMOVAL;
             type = "RELATIONSHIP";
-            propName = keys.stream().collect(Collectors.joining(","));
-            propValue = values.stream().collect(Collectors.joining(","));
 
             dataString += "Removed Relationship Properties | " + data.removedRelationshipProperties().toString() + " | ";
 
@@ -240,11 +182,9 @@ public class Neo4JTransactionEventListener implements TransactionEventListener<O
 
     {
         try {
-            Files.writeString(logFilePath, "\n///Commit time: " + data.getCommitTime(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
             commitTime = data.getCommitTime();
             dataString += "Commit Time | " + data.getCommitTime() + " | ";
 
-            Files.writeString(logFilePath, "\n///Transaction Id: " + data.getTransactionId(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
             transactionId = data.getTransactionId();
 
             dataString += "Transaction ID | " + data.getTransactionId() + " | ";
@@ -269,7 +209,7 @@ public class Neo4JTransactionEventListener implements TransactionEventListener<O
 
             }
 
-        } catch (IOException | SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
