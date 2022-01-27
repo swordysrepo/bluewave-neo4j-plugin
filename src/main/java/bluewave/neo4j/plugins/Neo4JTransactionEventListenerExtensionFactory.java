b64f3f1f86e39b6c71bcb0a07375ccd1a394f56a
pborissow <peter.borissow@yahoo.com>
2022-01-27 14:01:02 -0500

    Updated logic used to instantiate the Metadata class and added logic to instantiate the Logger class

--- src/main/java/bluewave/neo4j/plugins/Neo4JTransactionEventListenerExtensionFactory.java
+++ src/main/java/bluewave/neo4j/plugins/Neo4JTransactionEventListenerExtensionFactory.java
@@ -12,103 +12,221 @@ import org.neo4j.kernel.lifecycle.Lifecycle;
 import org.neo4j.kernel.lifecycle.LifecycleAdapter;
 import org.neo4j.logging.internal.LogService;
 
+import javaxt.json.JSONObject;
+import static javaxt.utils.Console.console;
+
+
+//******************************************************************************
+//**  Neo4JTransactionEventListenerExtensionFactory Class
+//******************************************************************************
+/**
+ *   Entry point for the plugin. Used to watch for server events.
+ *
+ ******************************************************************************/
+
 @ServiceProvider
 public class Neo4JTransactionEventListenerExtensionFactory
-        extends ExtensionFactory<Neo4JTransactionEventListenerExtensionFactory.Dependencies> {
+    extends ExtensionFactory<Neo4JTransactionEventListenerExtensionFactory.Dependencies> {
 
+    private Logger logger;
+    private Metadata meta;
+
+
+  //**************************************************************************
+  //** Constructor
+  //**************************************************************************
+    public Neo4JTransactionEventListenerExtensionFactory() {
+        super(ExtensionType.DATABASE, "neo4JTransactionEventHandler");
+    }
+
+
+  //**************************************************************************
+  //** newInstance
+  //**************************************************************************
+  /** Used to instantiate a Lifecycle listener
+   */
     @Override
-    public Lifecycle newInstance(final ExtensionContext extensionContext,
-            final Dependencies dependencies) {
+    public Lifecycle newInstance(final ExtensionContext extensionContext, final Dependencies dependencies) {
+
+
         final GraphDatabaseAPI db = dependencies.graphdatabaseAPI();
         final LogService log = dependencies.log();
-        final DatabaseManagementService databaseManagementService =
-                dependencies.databaseManagementService();
+        final DatabaseManagementService databaseManagementService = dependencies.databaseManagementService();
         final AvailabilityGuard availabilityGuard = dependencies.availabilityGuard();
-        final Metadata meta = new Metadata(db);
 
+
+
+      //Find and parse config file
+        javaxt.io.Jar jar = new javaxt.io.Jar(this);
+        java.io.File pluginDir = jar.getFile().getParentFile();
+        javaxt.io.File configFile = new javaxt.io.File(pluginDir, "config.json");
+        JSONObject config = null;
+        try { config = new JSONObject(configFile.getText()); }
+        catch (Exception e) { console.log(e.getMessage()); }
+
+
+
+      //Instantiate logger and metadata classes
+        if (config!=null){
+            try { logger = getLogger(config.get("logging").toJSONObject()); }
+            catch (Exception e) { console.log(e.getMessage()); }
+
+            try { meta = getMetadata(config.get("metadata").toJSONObject(), db); }
+            catch (Exception e) { console.log(e.getMessage()); }
+        }
+
+
+      //
         availabilityGuard.addListener(new AvailabilityListener() {
 
             @Override
             public void available() {
-
                 if (dependencies.graphdatabaseAPI().databaseName().compareTo("system") != 0) {
 
-                    Thread metaThread = new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            meta.init();
-                        }
-                    });
-
-                    try {
-                        metaThread.start();
-                        metaThread.join();
-                        meta.startTimer();
-                    } catch (Exception e) {
+                    if (meta!=null){
+                        meta.init(); //this will hang the server for a bit
+                        new Thread(meta).start();
                     }
+                    if (logger!=null) new Thread(logger).start();
                 }
             }
 
             @Override
             public void unavailable() {
                 if (dependencies.graphdatabaseAPI().databaseName().compareTo("system") != 0) {
-
-                    if (meta != null) {
-                        meta.cancelTimer();
-                    }
+                    if (meta!=null) meta.stop();
                 }
             }
 
         });
-        return new CustomGraphDatabaseLifecycle(log, db, dependencies, databaseManagementService,
-                meta);
+        return new CustomGraphDatabaseLifecycle(log, db, dependencies, databaseManagementService, logger, meta);
     }
 
+
+  //**************************************************************************
+  //** Dependencies
+  //**************************************************************************
     interface Dependencies {
         GraphDatabaseAPI graphdatabaseAPI();
-
         DatabaseManagementService databaseManagementService();
-
         AvailabilityGuard availabilityGuard();
-
         LogService log();
     }
 
+
+  //**************************************************************************
+  //** CustomGraphDatabaseLifecycle
+  //**************************************************************************
     public static class CustomGraphDatabaseLifecycle extends LifecycleAdapter {
         private final GraphDatabaseAPI db;
         private LogService log;
         private Neo4JTransactionEventListener transactionEventhandler;
         private final DatabaseManagementService databaseManagementService;
+        private Logger logger;
         private Metadata metadata;
 
-        public CustomGraphDatabaseLifecycle(final LogService log, final GraphDatabaseAPI db,
-                final Dependencies dependencies,
-                final DatabaseManagementService databaseManagementService, Metadata metadata) {
+        public CustomGraphDatabaseLifecycle(
+            final LogService log,
+            final GraphDatabaseAPI db,
+            final Dependencies dependencies,
+            final DatabaseManagementService databaseManagementService,
+            Logger logger, Metadata metadata){
+
             this.db = db;
             this.databaseManagementService = databaseManagementService;
             this.log = log;
+            this.logger = logger;
             this.metadata = metadata;
         }
 
         @Override
         public void start() {
-            if (this.db.databaseName().compareTo("system") != 0) {
-                this.transactionEventhandler =
-                        new Neo4JTransactionEventListener(this.db, log, metadata);
-                this.databaseManagementService.registerTransactionEventListener(
-                        this.db.databaseName(), this.transactionEventhandler);
+            if (db.databaseName().compareTo("system") != 0) {
+                transactionEventhandler = new Neo4JTransactionEventListener(db, log, logger, metadata);
+                databaseManagementService.registerTransactionEventListener(
+                    db.databaseName(), transactionEventhandler
+                );
             }
         }
 
         @Override
         public void shutdown() {
-            this.databaseManagementService.unregisterTransactionEventListener(
-                    this.db.databaseName(), this.transactionEventhandler);
+            databaseManagementService.unregisterTransactionEventListener(
+                db.databaseName(), transactionEventhandler
+            );
         }
     }
 
-    public Neo4JTransactionEventListenerExtensionFactory() {
-        super(ExtensionType.DATABASE, "neo4JTransactionEventHandler");
 
+  //**************************************************************************
+  //** getLogger
+  //**************************************************************************
+    private Logger getLogger(JSONObject config) throws Exception {
+
+
+      //Get path to the log file directory
+        javaxt.io.Directory logDir;
+        try {
+            logDir = new javaxt.io.Directory(config.get("local").get("path").toString());
+        }
+        catch (Exception e) {
+            logDir = null;
+        }
+
+
+      //Get database config
+        javaxt.sql.Database database;
+        try {
+            JSONObject json = config.get("database").toJSONObject();
+            String path = json.get("path").toString().replace("\\", "/");
+            javaxt.io.Directory dbDir = new javaxt.io.Directory(path);
+            dbDir.create();
+            path = new java.io.File(dbDir.toString() + "database").getCanonicalPath();
+
+            database = new javaxt.sql.Database();
+            database.setDriver("H2");
+            database.setHost(path);
+            database.setConnectionPoolSize(25);
+        }
+        catch (Exception e) {
+            database = null;
+        }
+
+      //Get webserver config
+        JSONObject webserver;
+        try {
+            webserver = config.get("webserver").toJSONObject();
+        }
+        catch (Exception e) {
+            webserver = null;
+        }
+
+
+        if (logDir==null && database==null && webserver==null) return null;
+
+
+      //Instantiate logger
+        Logger logger = new Logger();
+        if (logDir!=null) logger.setDirectory(logDir);
+        if (database!=null) logger.setDatabase(database);
+        if (webserver!=null) logger.setWebServer(webserver);
+        return logger;
+    }
+
+
+  //**************************************************************************
+  //** getMetadata
+  //**************************************************************************
+    private Metadata getMetadata(JSONObject config, GraphDatabaseAPI db){
+
+        Metadata metadata = new Metadata(db);
+
+        try{ metadata.setNodeName(config.get("node").toString()); }
+        catch(Exception e){}
+
+        try{ metadata.setCacheDirectory(new javaxt.io.Directory(config.get("localCache").toString())); }
+        catch(Exception e){}
+
+        return metadata;
     }
 }
